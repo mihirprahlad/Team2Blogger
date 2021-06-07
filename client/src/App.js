729943b43c517aa95faccdcfbe4957f8d4f16f67
jmg2akg <jmg2akg@virginia.edu>
2021-06-07 14:42:18 -0400

    Home component

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import BlogPage from './components/BlogPage'
 import Forum from "./components/Forum/Forum";
 import Store from "./components/Store/Store";
 import About from "./components/About/About";
-import LazyHero from 'react-lazy-hero';
+import Home from "./components/Home/Home";
 
 
 function App() {
@@ -13,16 +13,8 @@ function App() {
     <Router className="App">
       <main>
         <Navigation />
-      <LazyHero imageSrc="https://wallpaperaccess.com/full/647493.jpg" 
-              opacity={0.1}>
-                  <h1 style={{fontFamily: "Verdana", fontSize: 40}}>The Camille Connection</h1>
-
-              </LazyHero>
-              <h1 style={{fontFamily: "Verdana", margin: "50px"}}>About</h1>
-              <h2 style={{fontFamily: "Verdana", margin: "50px"}}>Content here </h2>
-              {}
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home}/>
           <Route path="/blog" component={()=><BlogPage/>}/>
           <Route path="/forum" component={Forum}/>
           <Route path="/store" component={Store}/>
