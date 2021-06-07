b17913dcaa7a8c1dde0a85ac221abf1e2ab489d1
A-Gagin <afg4pr@virginia.edu>
2021-06-07 13:19:22 -0400

    Added files for each page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Blog from "./components/Blog/Blog";
+import Forum from "./components/Forum/Forum";
+import Store from "./components/Store/Store";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -9,10 +13,10 @@ function App() {
         <Navigation />
         <Switch>
           <Route path="/" exact />
-          <Route path="/blog" />
-          <Route path="/forum" />
-          <Route path="/store" />
-          <Route path="/about" />
+          <Route path="/blog" component={Blog}/>
+          <Route path="/forum" component={Forum}/>
+          <Route path="/store" component={Store}/>
+          <Route path="/about" component={About}/>
         </Switch>
       </main>
     </Router>
