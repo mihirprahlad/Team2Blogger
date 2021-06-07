b9d783939bf227b7109e14565676661f0dba9367
jmg2akg <jmg2akg@virginia.edu>
2021-06-07 13:58:48 -0400

    Banner and text

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import LazyHero from 'react-lazy-hero';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div>
+            <LazyHero imageSrc="https://wallpaperaccess.com/full/647493.jpg" 
+            opacity={0.1}>
+                <h1 style={{fontFamily: "Verdana", fontSize: 40}}>The Camille Connection</h1>
+                
+            </LazyHero>
+            <h1 style={{fontFamily: "Verdana", margin: "50px"}}>About</h1>
+            <h2 style={{fontFamily: "Verdana", margin: "50px"}}>Content here </h2>
+            {}
+        </div>
   );
 }
 
