13deaf27840baea219c76c509c1e12c0b93eb69b
Andrew Niu <andrewmniu@gmail.com>
2021-06-08 14:24:35 -0400

    Used Prettier to format code

--- client/src/components/Store/DisplayItems.js
+++ client/src/components/Store/DisplayItems.js
@@ -1,49 +1,57 @@
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
-import Button from 'react-bootstrap/Button';
-import EditItems from './EditItems';
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import Button from "react-bootstrap/Button";
+import EditItems from "./EditItems";
 import { RiDeleteBinLine } from "react-icons/ri";
 
 export default function DisplayItems(props) {
-
-    return (
-        <div className="CardContainer" style={{ margin: "auto", width: "50%" }}>
-            <CardDeck>
-
-                {props.items.map((item) => (
-                    <Card>
-                        <Card.Img variant="top" src={item.image} style={{ height: "220px", width: "220px" }} />
-                        <Card.Body>
-                            <div>
-                                <EditItems name={item.name} description={item.description} price={item.price} image={item.image} id={item.id}/>
-                                <RiDeleteBinLine onClick={() =>
-                                    fetch(`http://localhost:5000/items/${item.id}`, {
-                                        method: "DELETE",
-                                        headers: {
-                                            Accept: "application/json",
-                                            "Content-Type": "application/json",
-                                        }
-                                    })
-                                        .then((obj) => {
-                                            console.log("deleting", obj);
-                                            window.location.reload();
-                                        })
-                                } />
-                            </div>
-                            <Card.Title>{item.name}</Card.Title>
-                            <Card.Text>{item.description}</Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <medium className="text-muted">$ {item.price}</medium>
-                            <Button size="sm" style={{ float: "right" }}>Add to Cart</Button>
-                            {/* ^ so far doesn't do anything, dummy button */}
-                        </Card.Footer>
-                    </Card>
-
-                ))}
-
-
-            </CardDeck>
-        </div>
-    );
+  return (
+    <div className="CardContainer" style={{ margin: "auto", width: "50%" }}>
+      <CardDeck>
+        {props.items.map((item) => (
+          <Card>
+            <Card.Img
+              variant="top"
+              src={item.image}
+              style={{ height: "220px", width: "220px" }}
+            />
+            <Card.Body>
+              <div>
+                <EditItems
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                  id={item.id}
+                />
+                <RiDeleteBinLine
+                  onClick={() =>
+                    fetch(`http://localhost:5000/items/${item.id}`, {
+                      method: "DELETE",
+                      headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                      },
+                    }).then((obj) => {
+                      console.log("deleting", obj);
+                      window.location.reload();
+                    })
+                  }
+                />
+              </div>
+              <Card.Title>{item.name}</Card.Title>
+              <Card.Text>{item.description}</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <medium className="text-muted">$ {item.price}</medium>
+              <Button size="sm" style={{ float: "right" }}>
+                Add to Cart
+              </Button>
+              {/* ^ so far doesn't do anything, dummy button */}
+            </Card.Footer>
+          </Card>
+        ))}
+      </CardDeck>
+    </div>
+  );
 }
