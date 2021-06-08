7664e1da9f4afbcee8ad38104adb4203025546d5
Mihir <55668536+mp8dy@users.noreply.github.com>
2021-06-08 09:06:21 -0400

    Cleaned up Comments component - removed unused code

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import Disqus, {DiscussionEmbed} from "disqus-react"
+import React from "react"
+import {DiscussionEmbed} from "disqus-react"
 
 export default function Comments(url, id, title) {
     console.log(`http://localhost:3000${url}`)
@@ -9,34 +9,11 @@ export default function Comments(url, id, title) {
         this.page.title = title;
     };
 
-    (function() {
-        const d = document, s = d.createElement("script");
-
-        s.src = "//thecamilleconnection.disqus.com/embed.js";
-        s.setAttribute("data-timestamp", +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
-
-
-
-    // const disqusShortname = "thecamilleconnection"
-    // const disqusConfig = {
-    //   url: `http://localhost:3000/${id}`,
-    //   identifier: id,
-    //   title: title
-    // }
-
     return (
         <div className="article-container">
             <DiscussionEmbed
                 shortname = "thecamilleconnection"
                 config = {{disqus_config}}
-                // config = {{
-                //     url: `http://localhost:3000${url}`,
-                //     identifier: id,
-                //     title: title,
-                //     categoryID: id
-                // }}
             />
 
         </div>
