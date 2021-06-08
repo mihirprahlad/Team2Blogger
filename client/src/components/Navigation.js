780c070f09f18fd3891684f7f8f3108ead747bf9
Andrew Niu <andrewmniu@gmail.com>
2021-06-08 10:17:51 -0400

    Enabled google login and logout

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,8 +1,34 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import firebase from "../auth.js";
 
 const Navigation = (props) => {
+  const [user, setUser] = useState(false);
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((res) => {
+        setUser(true);
+        return false;
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const logout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => setUser(false));
+  };
+
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand href="#home">Camille's Blog</Navbar.Brand>
@@ -24,6 +50,18 @@ const Navigation = (props) => {
           <Nav.Link as={Link} to="/about">
             About
           </Nav.Link>
+        </Nav>
+        <Nav>
+          {user ? (
+            <>
+              <Nav.Link as="p" className="mb-0">
+                Signed in as {firebase.auth().currentUser.displayName}
+              </Nav.Link>
+              <Button onClick={logout}>Logout</Button>
+            </>
+          ) : (
+            <Button onClick={signInWithGoogle}>Sign In</Button>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
