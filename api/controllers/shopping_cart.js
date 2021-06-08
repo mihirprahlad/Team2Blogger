822e41bc8501926693080ffec6f2e876f9c628e9
Andrew Niu <andrewmniu@gmail.com>
2021-06-08 17:24:43 -0400

    Added shopping cart DELETE endpoint

--- api/controllers/shopping_cart.js
+++ api/controllers/shopping_cart.js
@@ -1,10 +1,10 @@
 const ShoppingCartController = (app, db) => {
-  app.get("/users/:id/shopping-cart", async (req, res) => {
+  app.get("/users/:user_id/shopping-cart", async (req, res) => {
     let item_map;
     // Get item ids in user's shopping cart
     await db
       .collection("users")
-      .doc(req.params.id)
+      .doc(req.params.user_id)
       .get()
       .then((doc) => {
         item_map = doc.data().shopping_cart;
@@ -31,10 +31,10 @@ const ShoppingCartController = (app, db) => {
     res.json(cart);
   });
 
-  app.post("/users/:id/shopping-cart", async (req, res) => {
+  app.post("/users/:user_id/shopping-cart", async (req, res) => {
     let shopping_cart;
     const item_id = req.body.item_id;
-    const userDoc = db.collection("users").doc(req.params.id);
+    const userDoc = db.collection("users").doc(req.params.user_id);
 
     await userDoc.get().then((doc) => {
       shopping_cart = doc.data().shopping_cart;
@@ -43,6 +43,21 @@ const ShoppingCartController = (app, db) => {
     shopping_cart[item_id] = shopping_cart[item_id]
       ? shopping_cart[item_id] + 1
       : 1;
+
+    userDoc.update({ shopping_cart }).then(() => res.json(shopping_cart));
+  });
+
+  app.delete("/users/:user_id/shopping-cart/:item_id", async (req, res) => {
+    let shopping_cart;
+    const user_id = req.params.user_id;
+    const item_id = req.params.item_id;
+    const userDoc = db.collection("users").doc(req.params.user_id);
+
+    await userDoc.get().then((doc) => {
+      shopping_cart = doc.data().shopping_cart;
+    });
+
+    delete shopping_cart[item_id];
 
     userDoc.update({ shopping_cart }).then(() => res.json(shopping_cart));
   });
