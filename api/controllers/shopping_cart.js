310e1dc35d3acb0f9732d61155999630de211e3c
Andrew Niu <andrewmniu@gmail.com>
2021-06-09 10:00:14 -0400

    Added shopping cart PUT request

--- api/controllers/shopping_cart.js
+++ api/controllers/shopping_cart.js
@@ -34,6 +34,11 @@ const ShoppingCartController = (app, db) => {
   app.post("/users/:user_id/shopping-cart", async (req, res) => {
     let shopping_cart;
     const item_id = req.body.item_id;
+
+    if (!item_id) {
+      res.status(400).json({ msg: "Post request data not valid" });
+    }
+
     const userDoc = db.collection("users").doc(req.params.user_id);
 
     await userDoc.get().then((doc) => {
@@ -43,22 +48,40 @@ const ShoppingCartController = (app, db) => {
     shopping_cart[item_id] = shopping_cart[item_id]
       ? shopping_cart[item_id] + 1
       : 1;
-
     userDoc.update({ shopping_cart }).then(() => res.json(shopping_cart));
   });
+
+  app.put("/users/:user_id/shopping-cart/", async (req, res) => {
+    let shopping_cart;
+    const user_id = req.params.user_id;
+    const item_id = req.body.item_id;
+    const quantity = req.body.quantity;
+
+    if (!item_id || !quantity) {
+      res.status(400).json({ msg: "PUT request data not valid" });
+    }
+
+    const userDoc = db.collection("users").doc(user_id);
+
+    await userDoc.get().then((doc) => {
+      shopping_cart = doc.data().shopping_cart;
+    });
+
+    shopping_cart[item_id] = quantity
+    userDoc.update({ shopping_cart }).then(() => res.json(shopping_cart));
+  })
 
   app.delete("/users/:user_id/shopping-cart/:item_id", async (req, res) => {
     let shopping_cart;
     const user_id = req.params.user_id;
     const item_id = req.params.item_id;
-    const userDoc = db.collection("users").doc(req.params.user_id);
+    const userDoc = db.collection("users").doc(user_id);
 
     await userDoc.get().then((doc) => {
       shopping_cart = doc.data().shopping_cart;
     });
 
     delete shopping_cart[item_id];
-
     userDoc.update({ shopping_cart }).then(() => res.json(shopping_cart));
   });
 };
