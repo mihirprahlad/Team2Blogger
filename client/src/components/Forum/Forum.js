ba5a444f962d6a84c6fa1c9f03d456a007c2e940
Mihir <55668536+mp8dy@users.noreply.github.com>
2021-06-09 13:22:39 -0400

    Created beginnings of Forum page

--- client/src/components/Forum/Forum.js
+++ client/src/components/Forum/Forum.js
@@ -1,3 +1,47 @@
-export default function Forum(){
-    return "Forum page";
+import React, {useContext, useEffect, useState} from "react";
+import Spinner from "react-bootstrap/esm/Spinner";
+import PostCard from "./PostCard"
+import {UserContext} from "./../../contexts/UserContext"
+import SignIn from "./../SignIn"
+
+export default function Forum() {
+    const [posts, setPosts] = useState(null);
+    const {user} = useContext(UserContext);
+    
+    useEffect(() => {
+        console.log("useEffect");
+        console.log(user.photoURL)
+        fetch("http://localhost:5000/forumpost")
+            .then((res) => {
+                return(res.json())
+            })
+            .then((obj) => {
+                setPosts(obj);
+            });
+    }, [])
+
+    const display = <div style = {{paddingBottom: "5%"}}>
+        <h1 style = {{textAlign: "center", padding:"15px"}}>Hear from the fans!</h1>
+        {user ?
+            posts ? 
+                posts.map((post) => {
+                    return(
+                        <PostCard postContent = {post}/>
+                    )
+                })
+                :
+                <div class = "text-center">
+                    <Spinner animation = "grow"/>
+                </div>
+            :
+            <div class = "text-center">
+                <h3 style = {{padding:"15px"}}>You must log in to view forum posts!</h3>
+                <SignIn/>
+            </div>
+        }
+    </div>
+    
+    return(
+        display
+    );
 }
