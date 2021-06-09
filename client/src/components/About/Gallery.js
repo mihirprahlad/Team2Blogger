325caca20738369f9087d09c08f6d5bbaeba7e69
jmg2akg <jmg2akg@virginia.edu>
2021-06-09 16:18:30 -0400

    Image gallery added to about page

---
+++ client/src/components/About/Gallery.js
@@ -0,0 +1,20 @@
+import pic1 from './pic1.JPG';
+import pic2 from './pic2.JPG';
+import pic3 from './pic3.JPG';
+import React from 'react';
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
+const images = [
+    {
+      src: pic1
+    },
+    {
+      src: pic2
+    },
+    {
+      src: pic3
+    },
+  ];
+export default function Gallery() {
+    return <Carousel images={images} style={{ height: 620, width: 520 }} hasThumbnails={false} hasIndexBoard={false} hasSizeButton={false} hasMediaButton={false}/>
+}
