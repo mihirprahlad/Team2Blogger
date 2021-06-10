95fbc75a44ce218057aace270b6b87c612c7a91b
TJ Vitchutripop <79555155+tjdawgzone@users.noreply.github.com>
2021-06-10 16:26:04 -0400

    Update Navigation.js

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,26 +5,24 @@ import SignIn from "./SignIn.js";
 
 const Navigation = (props) => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Camille's Corner</Navbar.Brand>
     <Navbar style={{backgroundColor:"#003366"}}>
       <Link href="#home" style={{color:"beige",fontWeight:800,fontSize:28,paddingRight:30,paddingLeft:17}}>CAMILLE'S CORNER</Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">
+          <Nav.Link style={{color:"beige"}} as={Link} to="/">
             Home
           </Nav.Link>
-          <Nav.Link as={Link} to="/blog">
+          <Nav.Link style={{color:"beige"}} as={Link} to="/blog">
             Blog
           </Nav.Link>
-          <Nav.Link as={Link} to="/forum">
+          <Nav.Link style={{color:"beige"}} as={Link} to="/forum">
             Forum
           </Nav.Link>
-          <Nav.Link as={Link} to="/store">
+          <Nav.Link style={{color:"beige"}} as={Link} to="/store">
             Store
           </Nav.Link>
-          <Nav.Link as={Link} to="/about">
+          <Nav.Link style={{color:"beige"}} as={Link} to="/about">
             About
           </Nav.Link>
         </Nav>
@@ -32,7 +30,6 @@ const Navigation = (props) => {
           <SignIn />
         </Nav>
       </Navbar.Collapse>
-    </Navbar>
     </Navbar>
   );
 };
