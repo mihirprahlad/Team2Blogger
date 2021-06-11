2f6a15528d25b8fc4c3c9a7e1517b254a59d6b96
Teeratham Vitchutripop <teerathamvitchutripop@Teerathams-MBP.fios-router.home>
2021-06-11 09:20:15 -0400

    UI changes to Forum.  Added search bar functionality to Forum.

--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -57,12 +57,12 @@ export default function Blog(){
             <h1 style={{textAlign:"left",padding:"15px",fontSize:"80px"}}>BLOG.</h1>
             <div style={{paddingTop:"30px",paddingLeft:"43vw"}}>
             {user&&user.is_admin&&
-            <div style={{paddingLeft:120}}>
+            <div style={{paddingLeft:105}}>
                 <Button style={{height:50,backgroundColor:"#4C6357",border:"none"}} onClick={(e)=>{
             history.push("/newpost");
             e.stopPropagation();
             }}>New Post</Button></div>}
-            <Form.Control style={{marginTop:10}}name='value' value={search} onChange={(event) => { setSearch(event.target.value) }} onKeyPress={(evt) => {
+            <Form.Control style={{marginTop:(user&&user.is_admin?10:55),width:200,}}name='value' value={search} onChange={(event) => { setSearch(event.target.value) }} onKeyPress={(evt) => {
                     searchPosts();
                 }} placeholder={'Search by Title'} />
             </div>
