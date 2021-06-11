b0b7cffc608041f73ccde8a2de7ce55d54ea4625
Andrew Niu <andrewmniu@gmail.com>
2021-06-11 13:20:10 -0400

    Styled using prettier

--- client/src/components/Forum/ForumPost.js
+++ client/src/components/Forum/ForumPost.js
@@ -1,89 +1,137 @@
-import React,{useState,useEffect, useContext} from "react";
-import {useParams, useHistory} from "react-router-dom"
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import '../../App.css'
-import Spinner from 'react-bootstrap/Spinner'
+import React, { useState, useEffect, useContext } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import "../../App.css";
+import Spinner from "react-bootstrap/Spinner";
 import Likes from "./Likes.js";
 import { UserContext } from "../../contexts/UserContext";
-import SignIn from "./../SignIn"
-import Button from 'react-bootstrap/Button';
+import SignIn from "./../SignIn";
+import Button from "react-bootstrap/Button";
 import Comments from "../Comments/Comments";
-import moment from 'moment';
+import moment from "moment";
 
 export default function ForumPost() {
-    const history = useHistory();
-    const isLoggedIn = true;
-    const {forumID} = useParams();
-    const [content, setContent] = useState(null);
-    const {user} = useContext(UserContext);
+  const history = useHistory();
+  const isLoggedIn = true;
+  const { forumID } = useParams();
+  const [content, setContent] = useState(null);
+  const { user } = useContext(UserContext);
 
-    if(content){
-        console.log((content&&user&&(user.id===content.user.id))||(content&&user&&user.is_admin))
-    }
+  if (content) {
+    console.log(
+      (content && user && user.id === content.user.id) ||
+        (content && user && user.is_admin)
+    );
+  }
 
-    useEffect(() => {
-        fetch("http://localhost:5000/forumpost")
-            .then((res) => {
-                return(res.json())
-            })
-            .then((obj) => {
-                obj.forEach((post)=>{
-                    if(post.id===forumID){
-                        setContent(post);
+  useEffect(() => {
+    fetch("http://localhost:5000/forumpost")
+      .then((res) => {
+        return res.json();
+      })
+      .then((obj) => {
+        obj.forEach((post) => {
+          if (post.id === forumID) {
+            setContent(post);
+          }
+        });
+      });
+  }, []);
 
-                    }
-
-                })
-            })
-    },[])
-
-    const display = <div style={{marginLeft:"17%",marginRight:"17%",marginTop:"28px",marginBottom:"100px"}}>
-        {user ?
-            content ?
-                (<div>
-                    <div style={{right:"1%",top:"10%",position:"absolute"}}>
-                    {((content&&user&&(user.id===content.user.id))||(content&&user&&user.is_admin))?<Button style={{width:"5rem",marginBottom:"3px",backgroundColor:"#4C6357",border:"none"}}onClick={(e)=>{history.push("/editforumpost/"+content.id);
-                                    e.stopPropagation();}}>Edit</Button>:<div></div>}
-                    </div>
-                    <Row>
-                        <Col sm={10}>
-                            <h1 style={{fontWeight:"bold"}}>{content.title}</h1>
-                            <h4>{content.user.name}</h4>
-                            <h6>{moment(content.date).format("dddd, MMMM Do YYYY, h:mm:ss a")}</h6>
-                            {content.editDate!==""&&<h6>{moment(content.editDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}</h6>}
-                        </Col>
-                        <Col sm={2}>
-                            <Image style={{ width: '5rem'}} src={content.user.pic} roundedCircle/>
-                            <Likes
-                              postId={content.id}
-                              initialLikes={content.likes}
-                              initialDislikes={content.dislikes}
-                            />
-                      </Col>
-                    </Row>
-                    <Row style={{justifyContent:"center",marginBottom:"15px",marginTop:"5px"}}>
-                        <Image style={{ maxWidth: '50rem',maxHeight:"20rem"}} src={content.image}/>
-                    </Row>
-                    <div dangerouslySetInnerHTML={{ __html:content.content}} />
-                    {Comments(history.location.pathname, forumID, content.title)}
-                </div>)
-                :
-                (<div className="center">
-                        <Spinner animation="grow"/>
-                </div>)
-            :
-            <div className = "text-center">
-                <h3 style = {{padding:"15px"}}>You must log in to view forum posts!</h3>
-                <SignIn/>
+  const display = (
+    <div
+      style={{
+        marginLeft: "17%",
+        marginRight: "17%",
+        marginTop: "28px",
+        marginBottom: "100px",
+      }}
+    >
+      {user ? (
+        content ? (
+          <div>
+            <div style={{ right: "1%", top: "10%", position: "absolute" }}>
+              {(content && user && user.id === content.user.id) ||
+              (content && user && user.is_admin) ? (
+                <Button
+                  style={{
+                    width: "5rem",
+                    marginBottom: "3px",
+                    backgroundColor: "#4C6357",
+                    border: "none",
+                  }}
+                  onClick={(e) => {
+                    history.push("/editforumpost/" + content.id);
+                    e.stopPropagation();
+                  }}
+                >
+                  Edit
+                </Button>
+              ) : (
+                <div></div>
+              )}
             </div>
-        }
+            <Row>
+              <Col sm={10}>
+                <h1 style={{ fontWeight: "bold" }}>{content.title}</h1>
+                <h4>{content.user.name}</h4>
+                <h6>
+                  {moment(content.date).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+                </h6>
+                {content.editDate !== "" && (
+                  <h6>
+                    {moment(content.editDate).format(
+                      "dddd, MMMM Do YYYY, h:mm:ss a"
+                    )}
+                  </h6>
+                )}
+              </Col>
+              <Col sm={2}>
+                <Image
+                  style={{ width: "5rem" }}
+                  src={content.user.pic}
+                  roundedCircle
+                />
+                <Likes
+                  postId={content.id}
+                  initialLikes={content.likes}
+                  initialDislikes={content.dislikes}
+                />
+              </Col>
+            </Row>
+            <Row
+              style={{
+                justifyContent: "center",
+                marginBottom: "15px",
+                marginTop: "5px",
+              }}
+            >
+              <Image
+                style={{ maxWidth: "50rem", maxHeight: "20rem" }}
+                src={content.image}
+              />
+            </Row>
+            <div dangerouslySetInnerHTML={{ __html: content.content }} />
+            {Comments(history.location.pathname, forumID, content.title)}
+          </div>
+        ) : (
+          <div className="center">
+            <Spinner animation="grow" />
+          </div>
+        )
+      ) : (
+        <div className="text-center">
+          <h3 style={{ padding: "15px" }}>
+            You must log in to view forum posts!
+          </h3>
+          <SignIn />
+        </div>
+      )}
     </div>
+  );
 
-    return(
-        display
-    )
-
+  return display;
 }
--- client/src/components/Forum/PostCard.js
+++ client/src/components/Forum/PostCard.js
@@ -1,78 +1,110 @@
-import Card from 'react-bootstrap/Card';
-import Image from 'react-bootstrap/Image';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import React, {useState, useContext, useEffect} from 'react';
-import Button from 'react-bootstrap/Button'
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import React, { useState, useContext, useEffect } from "react";
+import Button from "react-bootstrap/Button";
 import { useHistory } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
 import Likes from "./Likes.js";
-import moment from 'moment';
+import moment from "moment";
 
-export default function PostCard({postContent}){
-  const {user} = useContext(UserContext);
-    const history = useHistory();
-    const [readMore,setReadMore] = useState(false);
+export default function PostCard({ postContent }) {
+  const { user } = useContext(UserContext);
+  const history = useHistory();
+  const [readMore, setReadMore] = useState(false);
 
-    const reduceContentLength = ((content) => {
-        content = content.replace(/<[^>]*>?/gm, '');
-        let displayContent = "";
-        if(content.length<=700) {
-            return content;
-        }
-        else {
-            for(let x=0;x<=700;x++) {
-                displayContent = displayContent+content.charAt(x);
-            }
-            return displayContent+"..."
-        }
-    });
-
-      function checkURL(url) {
-          return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+  const reduceContentLength = (content) => {
+    content = content.replace(/<[^>]*>?/gm, "");
+    let displayContent = "";
+    if (content.length <= 700) {
+      return content;
+    } else {
+      for (let x = 0; x <= 700; x++) {
+        displayContent = displayContent + content.charAt(x);
       }
+      return displayContent + "...";
+    }
+  };
 
-    return(
-        <div style={{paddingBottom:"3%"}}>
-            <Card style={{ width: '70vw' , height:'auto', margin:"auto"}}>
-                <Card.Body>
-                    <Container>
-                        <Row>
-                            <Col md={2}>
-                                <Image style={{ width: '5rem'}} src={postContent.user.pic} roundedCircle/>
-                            </Col>
-                            <Col md={8} style={{paddingLeft:"3%"}}>
-                                <h2 style={{fontWeight:"bold"}}>{postContent.title}</h2>
-                                <Card.Subtitle className="mb-2 text-muted">{postContent.user.name}</Card.Subtitle>
-                                <Card.Subtitle className="mb-2 text-muted">{moment(postContent.date).format("dddd, MMMM Do YYYY, h:mm:ss a")}</Card.Subtitle>
-                                {postContent.editDate !== "" && <Card.Subtitle style = {{fontStyle:"italic"}} className="mb-2 text-muted">Updated: {moment(postContent.editDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}</Card.Subtitle>}
-                            </Col>
-                            <Col md={2}>
-                              <Likes
-                                postId={postContent.id}
-                                initialLikes={postContent.likes}
-                                initialDislikes={postContent.dislikes}
-                              />
-                            </Col>
-                        </Row>
-                    {postContent.image !== "" && <div><Row style={{justifyContent:"center"}}>
-                        <Image class="img-fluid" style={{ maxWidth: '60vw',height:"330px"}} src={postContent.image}/>
-                    </Row></div>}
-                    <Row>
-                        <p style={{textAlign:"left",paddingTop:"2%",fontSize:15}}>{reduceContentLength(postContent.content)}</p>
-                    </Row>
-                    <Row style={{justifyContent:"center",paddingTop:"10px"}}>
-                        <Button style={{backgroundColor:"#4C6357",border:"none"}} onClick={(e)=>{
-                            history.push("/forumpost/"+postContent.id);
-                            e.stopPropagation();
-                        }}>{postContent.content.length>=700?"Read More":"Read"}</Button>
-                    </Row>
-                    </Container>
-                </Card.Body>
-            </Card>
+  function checkURL(url) {
+    return url.match(/\.(jpeg|jpg|gif|png)$/) != null;
+  }
 
-        </div>
-    )
-
+  return (
+    <div style={{ paddingBottom: "3%" }}>
+      <Card style={{ width: "70vw", height: "auto", margin: "auto" }}>
+        <Card.Body>
+          <Container>
+            <Row>
+              <Col md={2}>
+                <Image
+                  style={{ width: "5rem" }}
+                  src={postContent.user.pic}
+                  roundedCircle
+                />
+              </Col>
+              <Col md={8} style={{ paddingLeft: "3%" }}>
+                <h2 style={{ fontWeight: "bold" }}>{postContent.title}</h2>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {postContent.user.name}
+                </Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {moment(postContent.date).format(
+                    "dddd, MMMM Do YYYY, h:mm:ss a"
+                  )}
+                </Card.Subtitle>
+                {postContent.editDate !== "" && (
+                  <Card.Subtitle
+                    style={{ fontStyle: "italic" }}
+                    className="mb-2 text-muted"
+                  >
+                    Updated:{" "}
+                    {moment(postContent.editDate).format(
+                      "dddd, MMMM Do YYYY, h:mm:ss a"
+                    )}
+                  </Card.Subtitle>
+                )}
+              </Col>
+              <Col md={2}>
+                <Likes
+                  postId={postContent.id}
+                  initialLikes={postContent.likes}
+                  initialDislikes={postContent.dislikes}
+                />
+              </Col>
+            </Row>
+            {postContent.image !== "" && (
+              <div>
+                <Row style={{ justifyContent: "center" }}>
+                  <Image
+                    class="img-fluid"
+                    style={{ maxWidth: "60vw", height: "330px" }}
+                    src={postContent.image}
+                  />
+                </Row>
+              </div>
+            )}
+            <Row>
+              <p style={{ textAlign: "left", paddingTop: "2%", fontSize: 15 }}>
+                {reduceContentLength(postContent.content)}
+              </p>
+            </Row>
+            <Row style={{ justifyContent: "center", paddingTop: "10px" }}>
+              <Button
+                style={{ backgroundColor: "#4C6357", border: "none" }}
+                onClick={(e) => {
+                  history.push("/forumpost/" + postContent.id);
+                  e.stopPropagation();
+                }}
+              >
+                {postContent.content.length >= 700 ? "Read More" : "Read"}
+              </Button>
+            </Row>
+          </Container>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
