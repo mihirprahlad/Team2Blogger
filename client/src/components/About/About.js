c4d8d17443560299c6120a77cbf79af2ecf3d604
jmg2akg <jmg2akg@virginia.edu>
2021-06-11 09:02:18 -0400

    Small text and formatting changes

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,17 +1,12 @@
 import Gallery from './Gallery';
 
-// const imageStyle = {
-//     margin: "20px",
-//     height: "350px",
-//     width: "525px"
-// };
 const aboutTextStyle = {
     marginLeft: "-30px",
     fontSize: 20
 };
 const historyHeaderStyle = {
     marginLeft: "-30px",
-    fontSize: 40
+    fontSize: 50
 };
 
 const historyTextStyle = {
@@ -33,11 +28,11 @@ export default function About() {
                 </div>
                 <div class='column'>
                     <div>
-                        <b style={historyHeaderStyle}>About</b>
-                        <p style={aboutTextStyle}>Camille is a recent college graduate from UVA currently traveling from city to city exploring what urban life offers while taking advantage of any opportunity to get back to the great outdoors. Exclusively working for new start-ups as a freelancer, she has been able to have extended stays in NYC, Denver, SF, Seattle, and Barcelona. She loves being a digital nomad because each new city brings new stories and opportunities. </p>
+                        <h1 style={historyHeaderStyle}>About.</h1>
+                        <div style={{marginLeft: "-30px", marginBottom:20, height:15,width:155,backgroundColor:"#4C6357"}}></div>
+                        <p style={aboutTextStyle}>Camille is a recent college graduate from UVA currently traveling from city to city exploring what urban life offers while taking advantage of any opportunity to get back to the great outdoors. Educated as a chemical engineer, Camille threw that into the wind to pursue software development and adventure. Exclusively working for new start-ups as a freelancer, she has been able to have extended stays in NYC, Denver, San Francisco, Seattle, and Barcelona. She hopes to go next to Austin or London, but, in truth, with her, you never know where she'll be next. She loves being a digital nomad because each new city brings new stories and opportunities. </p>
                         <p style={aboutTextStyle}>Camille is a Virgo Libra cusp. Her hobbies include hiking, climbing, yoga, snowboarding, and anything with music. Her favorite  parts of nature are trees, mountains, and rivers; her favorite parts of cities are music and rooftops. </p>
                         <p style={historyTextStyle}></p>
-                        <div style={{height:15,width:110,backgroundColor:"#4C6357",marginLeft: "-30px",}}></div>
                     </div>
                 </div>
             </div>
